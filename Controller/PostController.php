b68e16b3807d9c439f9f18a5fadf22814c8a9403
emeraldMaster <constantine.emeraldmaster@gmail.com>
2012-04-09 17:32:03 +0300

    Upload images in blog post

--- Controller/PostController.php
+++ Controller/PostController.php
@@ -205,4 +205,46 @@ class PostController extends Controller
         return array('posts' => $posts);
     }
 
+
+    /**
+     * Upload photo
+     *
+     * @return string
+     * @Route("/admin/blog/uploadImage", name="blog_post_upload_image")
+     */
+    public function uploadImageAction()
+    {
+        $fileSize = (int)$_SERVER["CONTENT_LENGTH"];
+
+        $file = $_GET['qqfile'];
+        $pathinfo = pathinfo($file);
+        $filename = $pathinfo['filename'];
+        $ext = strtolower($pathinfo['extension']);
+        if (in_array($ext, array('jpg', 'jpeg', 'gif', 'png'))) {
+            $newName = uniqid() . '.' . $ext;
+            $input = fopen("php://input", "r");
+            $temp = tmpfile();
+            $realSize = stream_copy_to_stream($input, $temp);
+            fclose($input);
+
+            $uploadDir = realpath($this->get('kernel')->getRootDir() . '/../web/uploads/images');
+
+            $target = fopen($uploadDir . '/' . $newName, "w");
+            fseek($temp, 0, SEEK_SET);
+            stream_copy_to_stream($temp, $target);
+            fclose($target);
+
+            $resopnse = array(
+                'success' => true,
+                'filename' => $newName,
+            );
+        } else {
+            $resopnse = array(
+                'success' => false,
+                'error' => 'file extension is not valid'
+            );
+        }
+
+        return new Response(json_encode($resopnse));
+    }
 }
